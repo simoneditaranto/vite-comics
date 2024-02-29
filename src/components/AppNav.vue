12380f8265ec286b7dcd71682b07839cfb19796b
s.ditaranto <simodita99@gmail.com>
2024-02-29 14:06:05 +0100

    struttura navbar completata

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -10,6 +10,26 @@ export default {
     
     <nav>
 
+        <div class="container">
+            
+            <img src="/img/dc-logo.png" alt="dc logo">
+
+            <ul>
+
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+
+            </ul>
+
+        </div>
         
     </nav>
 
@@ -18,9 +38,30 @@ export default {
 <style>
 
 nav{
-    height: 120px;
+    /* height: 120px; */
     /* test */
 
     background-color: white;
+}
+
+.container{
+    max-width: 1250px;
+
+    margin: 0 auto;
+}
+
+nav .container{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 20px 0;
+}
+
+nav .container ul{
+    display: flex;
+    gap: 30px;
+
+    list-style-type: none;
 }
 </style>

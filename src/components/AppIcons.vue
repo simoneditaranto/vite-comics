5c1119d361788f53f4f3dd86cf4f4d56e27d88cc
s.ditaranto <simodita99@gmail.com>
2024-02-29 16:47:43 +0100

    sezione "AppIcons" completata

--- src/components/AppIcons.vue
+++ src/components/AppIcons.vue
@@ -1,7 +1,36 @@
 <script>
 
 export default{
-    name: 'AppIcons'
+    name: 'AppIcons',
+
+    data() {
+        return{
+
+            iconCards: [
+                {
+                    iconImg: '/img/buy-comics-digital-comics.png',
+                    iconTitle: 'digital comics',
+                },
+                {
+                    iconImg: '/img/buy-comics-merchandise.png',
+                    iconTitle: 'dc merchandise',
+                },
+                {
+                    iconImg: '/img/buy-comics-subscriptions.png',
+                    iconTitle: 'subscription',
+                },
+                {
+                    iconImg: '/img/buy-comics-shop-locator.png',
+                    iconTitle: 'comic shop locator',
+                },
+                {
+                    iconImg: '/img/buy-dc-power-visa.svg',
+                    iconTitle: 'dc power visa',
+                },
+            ]
+
+        }
+    }
 }
 
 </script>
@@ -10,17 +39,53 @@ export default{
 
     <div id="icons">
 
+        <div class="cards">
+
+            <div class="card" v-for="currentCard in iconCards">
+                <img :src="currentCard.iconImg" alt="">
+                <div class="icon-text">
+                    {{ currentCard.iconTitle }}
+                </div>
+            </div>
+
+        </div>
+
+
     </div>
 
 </template>
 
-<style>
+<style lang="scss">
 
 #icons{
-    height: 150px;
-    /* test */
+
+    padding: 50px 0;
 
     background-color: #0282F9;
+
+    .cards {
+        display: flex;
+        justify-content: center;
+        gap: 70px;
+
+        .card{
+            display: flex;
+            align-items: center;
+            gap: 15px;
+
+            img{
+                width: 50px;
+                height: 50px;
+                object-fit: contain;
+            }
+
+            .icon-text {
+                color: white;
+
+                text-transform: uppercase;
+            }
+        }
+    }
 }
 
 </style>

b1d8ca2072ef168b0dc621217e7b2a142534f415
s.ditaranto <simodita99@gmail.com>
2024-03-01 13:46:08 +0100

    aggiunta cards fumetti completato

---
+++ src/components/ComicItem.vue
@@ -0,0 +1,57 @@
+<script>
+
+export default {
+    name: 'ComicItem',
+
+    // dichiaro le proprietà del mio item
+    props: {
+        // titolo della serie
+        comicSeries: String,
+        // url dell'immagine
+        comicImage: String,
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="comic-card">
+        <div class="comic-img">
+            <img :src="comicImage" alt="">
+        </div>
+        <span>{{ comicSeries }}</span>
+    </div>
+
+</template>
+
+<style lang="scss">
+
+.comic-card{
+
+    width: calc(100% / 6 - 25px / 6 * 5);
+
+    .comic-img {
+
+        margin-bottom: 10px;
+
+        width: 100%;
+        
+        img{
+            width: 100%;
+            aspect-ratio: 1 / 1;
+            object-fit: cover;
+            object-position: top;
+        }
+
+    }
+
+
+    span{
+        color: white;
+
+        text-transform: uppercase;
+    }
+}
+
+</style>
